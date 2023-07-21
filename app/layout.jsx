c298a06dea77fd1e3e76d6d477ca4a37da1d74cf
Natalie Gillam <ngillam@hotmail.co.uk>
2023-07-21 09:33:17 +0100

    added layout and page components

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/globals.css"
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover & Share AI Prompts"
+  }
+  
+
+function RootLayout() {
+  return (
+    <html lang="en">
+    <body>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+    </body>
+  </html>
+
+  )
+}
+
+export default RootLayout
