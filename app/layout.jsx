f3c84f1bc38221cf3ed073070a191fd06592912e
Natalie Gillam <ngillam@hotmail.co.uk>
2023-07-23 23:37:50 +0100

    added main section and 'children' prop to layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,13 +6,16 @@ export const metadata = {
   }
   
 
-function RootLayout() {
+function RootLayout({children}) {
   return (
     <html lang="en">
     <body>
       <div className="main">
         <div className="gradient" />
       </div>
+      <main className="app">
+        {children}
+      </main>
     </body>
   </html>
 
