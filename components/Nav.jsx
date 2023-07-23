6a94d18e7adda629c3d0d4fd36c84406aa079434
Natalie Gillam <ngillam@hotmail.co.uk>
2023-07-24 00:38:42 +0100

    added logo to Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from 'next/link';
 import Image from 'next/image'
 import {useState, useEffect} from 'react';
@@ -8,7 +10,11 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image src="/assets/images/logo.svg" alt="Promptopia logo" width={30} height={30} className="object-contain"/>
+      </Link>
+    </nav>
   )
 }
 
