83d3e583aefa6f31e811f8aef81734aade1d8d1c
Natalie Gillam <ngillam@hotmail.co.uk>
2023-07-23 23:40:32 +0100

    added h1 tag and first paragraph to page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for users to discover, create and share creative prompts.
+      </p>
+      {/* component feed will go here */}
+    </section>
+  );
 }
 
-export default page
+export default Home;
